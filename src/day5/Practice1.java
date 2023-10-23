d3a8087e7af2bba07bea118f254b5e466f7ac22b
Abel Shin <abel@even-my-dogs.com>
2023-10-23 10:09:42 +0900

    bug fix

--- src/day5/Practice1.java
+++ src/day5/Practice1.java
@@ -7,14 +7,14 @@ package day5;
  */
 
 class Practice1 {
-    boolean dfs(int node, int target, int[][] edges) {
+    boolean dfs(int node, int target, int n, int[][] edges) {
         // node를 시작점으로 하여, target 노드를 탐색하세요.
         // 탐색 결과를 true/false로 반환하세요.
         // 동작 과정을 확인하기 위해 node에 접근할 때 마다 해당 노드의 번호를 콘솔에 출력하세요.
         return true;
     }
 
-    boolean bfs(int node, int target, int[][] edges) {
+    boolean bfs(int node, int target, int n, int[][] edges) {
         // node를 시작점으로 하여, target 노드를 탐색하세요.
         // 탐색 결과를 true/false로 반환하세요.
         // 동작 과정을 확인하기 위해 node에 접근할 때 마다 해당 노드의 번호를 콘솔에 출력하세요.
@@ -24,7 +24,7 @@ class Practice1 {
     public static void main(String[] args) {
         int n = 7;
         int[][] edges = {{1, 4}, {2, 3}, {5, 2}, {3, 6}, {4, 3}, {7, 6}, {6, 2}};
-        System.out.println(new Practice2().dfs(1, 6, n, edges));
-        System.out.println(new Practice2().bfs(1, 6, n, edges));
+        System.out.println(new Practice1().dfs(1, 6, n, edges));
+        System.out.println(new Practice1().bfs(1, 6, n, edges));
     }
 }
